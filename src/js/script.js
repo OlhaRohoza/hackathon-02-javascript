452c479f37614155413e8010bb2a1bfb4601b556
Indig0s <cyanide.om@gmail.com>
2022-05-27 10:27:15 +0200

    event fetch set

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
-console.log('Hello!');
+const loadData = async () => {
+  const response = await fetch(
+    "https://test-api.codingbootcamp.cz/api/65d16700/events"
+  );
+  const data = await response.json();
+  console.log(data);
+};
+console.log("hello");
+loadData();
